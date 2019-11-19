d562b4ba58b554836acf91b431dd496e178b2689
Tanner York <tannerwyork@gmail.com>
2019-11-18 16:50:18 -0800

    Add algorithm pages and changed templating system

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Index page of the application"""
-    return render_template('index.html')
+    return render_template('main.html', page="index")
 
+
+@app.route('/sorting')
+def sorting():
+    """Page for visualizing sorting algorythms"""
+    return render_template('main.html', page="sorting")
+
+@app.route('/pathfinding')
+def pathfinding():
+    """Page for visualizing sorting algorythms"""
+    return render_template('main.html', page="pathfinding")
 
 if __name__ == '__main__':
     app.run(debug=True)
