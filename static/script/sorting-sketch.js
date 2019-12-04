82b62417cec868cadeb559bd91cbfbb5d8c305a7
Tanner York <tannerwyork@gmail.com>
2019-12-03 18:00:47 -0800

    Add sorting visulization options

--- static/script/sorting-sketch.js
+++ static/script/sorting-sketch.js
@@ -1,37 +1,53 @@
-const winWidth = document.getElementById('sortingContainer').offsetWidth
-const winHeight = document.getElementById('sortingContainer').offsetHeight
-const arrayLength = 10
-const algorithm = document.getElementById('sorting-algorithm').value
-
+const winWidth = document.getElementById('sortingContainer').offsetWidth;
+const winHeight = document.getElementById('sortingContainer').offsetHeight;
+const sortingForm = document.getElementById('sorting-form');
+const sortingAlgorithm = document.getElementById('sorting-algorithm');
+const sortingLength = document.getElementById('sorting-length');
 
 function setup() {
-    var canvas = createCanvas(winWidth, winHeight);
-    canvas.parent('sortingContainer')
-    visualArray = new VisualArray(arrayLength);
-    frameRate(arrayLength)
-    sorting = new Sorting(visualArray.intArray)
+    canvas = createCanvas(winWidth, winHeight);
+    arrayLength = 10;
+    algorithm = 'bubble'
+    shouldDraw = false
+    visualArray = null
+    sorting = null
+
+    canvas.parent('sortingContainer');
+    frameRate(100);
+
+    sortingForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        visualArray = new VisualArray(sortingLength.value);
+        sorting = new Sorting(visualArray.intArray);
+        algorithm = sortingAlgorithm.value;
+        shouldDraw = true;
+    })
 }
 
 function draw() {
-    background(255);
-    visualArray.draw()
-    visualArray.update()
-    sorting.bubbleSort()
+    if (shouldDraw) {
+        background(255);
+        visualArray.draw();
+        visualArray.update();
+        if (algorithm == 'bubble') {
+            sorting.bubbleSort()
+        }
+    }
 }
 
 class Bar {
     constructor(value, height, width, x) {
         /** Creates an object thats used to render integers as rects */
-        this.value = value
+        this.value = value;
         this.height = height;
-        this.width = width
-        this.x = x
+        this.width = width;
+        this.x = x;
     }
   
     draw() {
         /** Draws a rect with the x, width, and height of the bar */
-        fill(105, 199, 211)
-        rect(this.x, winHeight, this.width, -this.height)
+        fill(105, 199, 211);
+        rect(this.x, winHeight, this.width, -this.height);
     }
 }
 
@@ -44,36 +60,36 @@ class VisualArray {
          *  Returns:
          *      VisualArray (object): An object capable of rendering an shuffled array on the canvas
          */
-        this.intArray = shuffle(new Array(arrayLength).fill(0).map((value, index) => index+1));
+        this.intArray = shuffle(new Array(int(arrayLength)).fill(0).map((value, index) => index+1));
         this.barWidth = winWidth/this.intArray.length;
         this.barHeight = winHeight/this.intArray.length;
-        this.barArray = this.createBarArray()
+        this.barArray = this.createBarArray();
     }
   
     createBarArray() {
         /** Creates an array of bar objects from the VisualArray's intArray */
-        var barArray = []
-        var x = 0
-        const maxValue = Math.max(...this.intArray)
+        var barArray = [];
+        var x = 0;
+        const maxValue = Math.max(...this.intArray);
         for (var i=0; i < this.intArray.length; i++) {
-            const value = this.intArray[i]
-            const height = ((winHeight-50)/maxValue)*value
+            const value = this.intArray[i];
+            const height = ((winHeight-50)/maxValue)*value;
             barArray.push(new Bar(value, height, this.barWidth, x));
-            x = x + this.barWidth
+            x = x + this.barWidth;
         }
-        return barArray
+        return barArray;
     }
 
     draw() {
         /** Draws each bar in the barArray onto the canvas */
         this.barArray.forEach(bar => {
-            bar.draw()
+            bar.draw();
         })
     }
   
     update() {
         /** Updates the bars in bar array */
-        this.barArray = this.createBarArray()
+        this.barArray = this.createBarArray();
     }
 }
 
